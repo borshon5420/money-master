e97a808ac4909f0663474a807670e9e73c3732dc
borshon5420 <dhruba.sen2020@gmail.com>
2022-02-17 23:46:22 +0100

    My git commit

---
+++ js/calculation.js
@@ -0,0 +1,7 @@
+document.getElementById('calculate-btn').addEventListener('click', function(){
+    
+    const incomeInput= document.getElementById('income-input');
+    const incomeText = incomeInput.value;
+    const incomeValue = parseFloat(incomeText);
+    
+})
