baf5a998debd163cac5fe15d7125733dfb04cb5f
borshon5420 <dhruba.sen2020@gmail.com>
2022-02-18 17:06:32 +0100

    almost

--- js/calculation.js
+++ js/calculation.js
@@ -5,53 +5,70 @@ function getInputValue (inputId) {
     return amountValue;
 }
 
-// function updateTotalField ()
+function updateTotalField (fieldId, amount1, amount2 ,amount3 ) {
 
+    const totalElement = document.getElementById(fieldId);
+    let totalElementAmount = parseFloat(totalElement.innerText);
+    totalElementAmount = amount1 + amount2 + amount3 ;
+    totalElement.innerText = totalElementAmount;
+    return totalElementAmount;
+}
 
+function updateBalance (balanaceId, income, expenses ) {
+    const balanceTotal =document.getElementById (balanaceId);
+    let balanceTotalAmount = parseFloat(balanceTotal.innerText);
+    balanceTotalAmount = income -expenses;
+    balanceTotal.innerText =balanceTotalAmount;
+    return balanceTotalAmount;
+}
 
-
+  
 
 document.getElementById('calculate-btn').addEventListener('click', function(){
    
     // Income Input 
     const incomeAmount = getInputValue ('income-input');
+    // if((isNaN(incomeAmount) || (incomeAmount <0))) {
+    //     alert ('Please give a postive number');
+
+    // }
     // Food Costing
     const foodCostingAmount = getInputValue ('food-costing');
+    // if((isNaN(foodCostingAmount) || (foodCostingAmount <0))) {
+    //     alert ('Please give a postive number');
+    // }
+
     //Rent Costing 
     const rentCostingAmount = getInputValue ('rent-costing');
+    // if((isNaN(rentCostingAmount) || (rentCostingAmount <0))) {
+    //     alert ('Please give a postive number');
+    // }
     // Clothing costing
     const clothCostingAmount = getInputValue ('cloth-costing');
-    
-    // Total Expense 
+    // if((isNaN(clothCostingAmount) || (clothCostingAmount <0))) {
+    //     alert ('Please give a postive number');
+    // }
 
-    const totalExpenses = document.getElementById('total-expenses');
-    let totalExpensesAmount = parseFloat(totalExpenses.innerText);
-    totalExpensesAmount = + foodCostingAmount + rentCostingAmount + clothCostingAmount ;
-    totalExpenses.innerText = totalExpensesAmount;
-    
+    // Total Expense 
+   const totalExpensesAmount= updateTotalField('total-expenses',foodCostingAmount,rentCostingAmount,clothCostingAmount);
+
    
 
     // Balance
+   const balanceAmount = updateBalance ('balance-total', incomeAmount, totalExpensesAmount);
+    //    if (balanceAmount.value <0){
+    //        alert("you don't have sufficient balance")
+    //    }
+    });
 
-    const balanceTotal =document.getElementById ('balance-total');
-    let balanceTotalAmount = parseFloat(balanceTotal.innerText);
-    balanceTotalAmount = incomeAmount -totalExpensesAmount;
-    balanceTotal.innerText =balanceTotalAmount;
-
-});
-
-// Save Buuton CLick
-document.getElementById ('save-btn').addEventListener ('click', function(){
+    // Save Buuton CLick
+    document.getElementById ('save-btn').addEventListener ('click', function(){
     
     // Save percentage
-    const savePercentageInput = document.getElementById ('save-input');
-    const savePercentageInputText = savePercentageInput.value;
-    const savePercentageInputAmount = parseFloat (savePercentageInputText);
+    const savePercentageInputAmount = getInputValue ('save-input');
    
     // Income
-    const incomeInput= document.getElementById('income-input');
-    const incomeText = incomeInput.value;
-    const incomeAmount = parseFloat(incomeText);
+    const incomeAmount = getInputValue ('income-input');
 
     // Saving amount
     const savingInput =document.getElementById('saving-amount');
@@ -73,3 +90,34 @@ document.getElementById ('save-btn').addEventListener ('click', function(){
 
 
 })
+
+function errorMessage() {
+    const incomeAmount =document.getElementById ('income-input').value;
+    const foodCostingAmount =document.getElementById ('food-costing').value;
+    const rentCostingAmount = document.getElementById ('rent-costing').value;
+    const clothCostingAmount =document.getElementById ('cloth-costing').value;
+    const totalExpensesAmount = document.getElementById ('total-expenses').innerText;
+    const balanceAmount = document.getElementById ('balance-total').innertext;
+
+    if ( (isNaN(incomeAmount) || (incomeAmount <0)) || (isNaN(foodCostingAmount) || (foodCostingAmount <0))) {
+        alert ('Please give a postive number');
+     totalExpensesAmount.innerText = ' ';
+
+    }
+  
+}
+
+function savingError () {
+    let error = document.getElementById("error")
+    const  savingInputAmount = document.getElementById ('saving-amount').innerText;
+    const balanceTotalAmount =document.getElementById('balance-total').innerText;
+    if (savingInputAmount < balanceTotalAmount) 
+    {
+        // Changing content and color of content
+        error.textContent = "You don't have sufficient balance"
+        error.style.color = "red"
+        savingInputAmount.innerText = ' ';
+    } else {
+        error.textContent = ""
+    }
+}
