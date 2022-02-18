a465daf55126e6d804e5829a3516c8e78c24fd73
borshon5420 <dhruba.sen2020@gmail.com>
2022-02-18 18:24:56 +0100

    99% done

--- js/calculation.js
+++ js/calculation.js
@@ -28,37 +28,36 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
    
     // Income Input 
     const incomeAmount = getInputValue ('income-input');
-    // if((isNaN(incomeAmount) || (incomeAmount <0))) {
-    //     alert ('Please give a postive number');
+    if((isNaN(incomeAmount) || (incomeAmount <0))) {
+        alert ('Please give a postive number');
 
-    // }
+    }
     // Food Costing
     const foodCostingAmount = getInputValue ('food-costing');
-    // if((isNaN(foodCostingAmount) || (foodCostingAmount <0))) {
-    //     alert ('Please give a postive number');
-    // }
+    if((isNaN(foodCostingAmount) || (foodCostingAmount <0))) {
+        alert ('Please give a postive number');
+    }
 
     //Rent Costing 
     const rentCostingAmount = getInputValue ('rent-costing');
-    // if((isNaN(rentCostingAmount) || (rentCostingAmount <0))) {
-    //     alert ('Please give a postive number');
-    // }
+    if((isNaN(rentCostingAmount) || (rentCostingAmount <0))) {
+        alert ('Please give a postive number');
+    }
     // Clothing costing
     const clothCostingAmount = getInputValue ('cloth-costing');
-    // if((isNaN(clothCostingAmount) || (clothCostingAmount <0))) {
-    //     alert ('Please give a postive number');
-    // }
+    if((isNaN(clothCostingAmount) || (clothCostingAmount <0))) {
+        alert ('Please give a postive number');
+    }
 
     // Total Expense 
    const totalExpensesAmount= updateTotalField('total-expenses',foodCostingAmount,rentCostingAmount,clothCostingAmount);
 
-   
-
     // Balance
-   const balanceAmount = updateBalance ('balance-total', incomeAmount, totalExpensesAmount);
-    //    if (balanceAmount.value <0){
-    //        alert("you don't have sufficient balance")
-    //    }
+   let balanceAmount = updateBalance ('balance-total', incomeAmount, totalExpensesAmount);
+       if (balanceAmount <0){
+           alert("Ooops!!! Youd don't have sufficient balance")
+            balanceAmount.innerText(0);
+       }
     });
 
     // Save Buuton CLick
@@ -74,50 +73,25 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
     const savingInput =document.getElementById('saving-amount');
     let savingInputAmount = parseFloat(savingInput.innerText);
     savingInputAmount = incomeAmount * (savePercentageInputAmount/100);
-    savingInput.innerText = savingInputAmount; 
+    savingInput.innerText = savingInputAmount;
+    
+  
 
     // Balance
 
     const balanceTotal =document.getElementById ('balance-total');
     let balanceTotalAmount = parseFloat(balanceTotal.innerText);
   
-
     // Remaining Balance
     const remainngBalance= document.getElementById('remaining-amount');
     let remainingBalanceAmount= parseFloat(remainngBalance.innerText);
     remainingBalanceAmount = balanceTotalAmount - savingInputAmount; 
     remainngBalance.innerText = remainingBalanceAmount;
 
+    // Savings error
+    if (savingInputAmount >  balanceTotalAmount) {
+        alert ("You don't have sufficient balance to save !!!")
+    }
 
 })
 
-function errorMessage() {
-    const incomeAmount =document.getElementById ('income-input').value;
-    const foodCostingAmount =document.getElementById ('food-costing').value;
-    const rentCostingAmount = document.getElementById ('rent-costing').value;
-    const clothCostingAmount =document.getElementById ('cloth-costing').value;
-    const totalExpensesAmount = document.getElementById ('total-expenses').innerText;
-    const balanceAmount = document.getElementById ('balance-total').innertext;
-
-    if ( (isNaN(incomeAmount) || (incomeAmount <0)) || (isNaN(foodCostingAmount) || (foodCostingAmount <0))) {
-        alert ('Please give a postive number');
-     totalExpensesAmount.innerText = ' ';
-
-    }
-  
-}
-
-function savingError () {
-    let error = document.getElementById("error")
-    const  savingInputAmount = document.getElementById ('saving-amount').innerText;
-    const balanceTotalAmount =document.getElementById('balance-total').innerText;
-    if (savingInputAmount < balanceTotalAmount) 
-    {
-        // Changing content and color of content
-        error.textContent = "You don't have sufficient balance"
-        error.style.color = "red"
-        savingInputAmount.innerText = ' ';
-    } else {
-        error.textContent = ""
-    }
-}
